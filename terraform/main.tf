b834e2e3b73bbe6db5ad3e52deaada0d8bd4970c
Harbey-tech <harbey2965@gmail.com>
2026-01-09 02:15:39 +0100

    Change instance size

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "bucket" {
 # EC2 instance
 resource "aws_instance" "ec2" {
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (us-east-1)
-  instance_type = var.instance_type
+  instance_type = "t3.small"
 
   tags = {
     Name = var.project_name
